34b78981944143e67c968fdca2aeaf4394005550
Thảo Đỗ Hồng <dohongthao@Thaos-MacBook-Pro.local>
2022-01-07 16:13:57 +0700

    change Status

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import '../styles/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+  <div className="h-screen bg-purple-700 text-gray-100">
+    <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
